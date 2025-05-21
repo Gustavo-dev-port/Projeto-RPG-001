d76265115b8da9243ff1c7922fe8e54740df5146
gustavo-dev <gustavo.developer.souza@gmail.com>
2025-05-21 17:25:02 -0300

    save0.01

--- sistema/SaveManager.java
+++ sistema/SaveManager.java
@@ -1,5 +1,32 @@
 package sistema;
 
-public class SaveManager {
+import java.nio.file.*;
+import utils.Criptografia;
 
+public class SaveManager {
+    private static final Path CAMINHO = Paths.get("textos", "save.txt");
+
+    public static void salvar(String conteudo) {
+        try {
+            String criptografado = Criptografia.criptografar(conteudo);
+            Files.writeString(CAMINHO, criptografado);
+            System.out.println("Jogo Salvo com sucesso!");
+        } catch (Exception e) {
+            System.out.println("Erro ao salvar o jogo: " + e.getMessage());
+        }
+
+    }
+
+    public static String carregar() {
+        try {
+            if (!Files.exists(CAMINHO))
+                return null;
+            String criptografado = Files.readString(CAMINHO);
+            return Criptografia.descriptografar(criptografado);
+
+        } catch (Exception e) {
+            System.out.println("Erro ao carregar o jogo: " + e.getMessage());
+            return null;
+        }
+    }
 }
--- utils/Criptografia.java
+++ utils/Criptografia.java
@@ -1,5 +1,26 @@
 package utils;
 
-public class Criptografia {
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
+public class Criptografia {
+    private static final String CHAVE = "1234567890123456"; // 16 bytes para AES-128
+
+    public static String criptografar(String texto) throws Exception {
+        SecretKeySpec chave = new SecretKeySpec(CHAVE.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, chave);
+        byte[] textoCriptografado = cipher.doFinal(texto.getBytes());
+        return Base64.getEncoder().encodeToString(textoCriptografado);
+    }
+
+    public static String descriptografar(String textoCriptografado) throws Exception {
+        SecretKeySpec chave = new SecretKeySpec(CHAVE.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, chave);
+        byte[] textoDecifrado = cipher.doFinal(Base64.getDecoder().decode(textoCriptografado));
+        return new String(textoDecifrado);
+
+    }
 }
